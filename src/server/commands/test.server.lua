ecabfdb4193a4a32270cacf820b483ca0d3c0c8c
decibel <vitormarques1469@gmail.com>
2025-09-21 12:41:35 -0300

    feat: /copy command + registercommand now returns player module

--- src/server/commands/test.server.lua
+++
@@ -1,9 +0,0 @@
-local RegisterCommand = require(game.ReplicatedStorage.Shared['register-command'])
-
-RegisterCommand({
-    name = "reset",
-    description = "comando de teste",
-    aliases = {"r", "res"}
-}, function(player, params, message)
-    print(player, 'executeou o comando')
-end)
